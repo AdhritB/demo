941b207c64175bc71ed4333129c46b062d5010df
Aditya007Datta <adityadattaofficial@gmail.com>
2023-02-16 21:09:28 +0530

    second commit

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -10,8 +10,16 @@ const Hero = () => {
         >
             <div className="hero min-h-screen bg-base-200">
                 <div className="hero-content flex-col lg:flex-row-reverse">
-                    <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpogd.es%2Fassets%2Fbg%2FMarvels-Spidey-and-His-Amazing-Friends.jpg&f=1&nofb=1&ipt=c41620af718cf8a0f744d8fc06bd8af1b13f13c67ae5fac51619d4b6e7b2adda&ipo=images" className="max-w-sm rounded-lg shadow-2xl" />
-                    <div>
+                    <motion.div
+
+                        animate={{
+                            scale: 0.5,
+                            transition: {
+                                duration: 1,
+                            },
+                        }}>
+                        <img src="https://th.bing.com/th/id/R.1b6e02d2c2d677f52d501f556f43bec3?rik=iRQegkfASuW0HA&riu=http%3a%2f%2fdreamicus.com%2fdata%2fvalley%2fvalley-01.jpg&ehk=Leg%2f6hDj%2bBLkAfFH910y6eCXtfvUEkqn13IG50OJ14E%3d&risl=&pid=ImgRaw&r=0" className="max-w-sm rounded-lg shadow-2xl" />
+                    </motion.div> <div>
                         <h1 className="text-5xl font-bold text-secondary">Box Office News!</h1>
                         <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
                         <button className="btn btn-primary">Get Started</button>
